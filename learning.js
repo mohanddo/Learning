f03ffbf4529dcacb206a5d6fd2daa3c5f9466013
mohanddo <93376862+mohanddo@users.noreply.github.com>
2024-02-10 11:24:33 +0000

    First commit

---
+++ learning.js
@@ -0,0 +1,26 @@
+
+// import { divide } from "./node_modules/lodash/divide.js";
+// console.log(divide)
+
+// const canvas = document.getElementById("canvas");
+// const ctx = canvas.getContext("2d");
+
+// for (let i = 0; i < 4; i++) {
+//     for (let j = 0; j < 3; j++) {
+//       ctx.beginPath();
+//       const x = 25 + j * 50; // x coordinate
+//       const y = 25 + i * 50; // y coordinate
+//       const radius = 20; // Arc radius
+//       const startAngle = 0; // Starting point on circle
+//       const endAngle = Math.PI + (Math.PI * j) / 2; // End point on circle
+//       const counterclockwise = i % 2 !== 0; // clockwise or counterclockwise
+
+//       ctx.arc(x, y, radius, startAngle, endAngle, counterclockwise);
+
+//       if (i > 1) {
+//         ctx.fill();
+//       } else {
+//         ctx.stroke();
+//       }
+//     }
+//   }
